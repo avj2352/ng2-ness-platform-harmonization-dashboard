763a3cdc0a3f2a3ae9f84374c833a70f02a9e602
avj2352 <pramod.jingade@philips.com>
2018-01-23 20:55:06 +0530

    Integrated smart table

--- src/app/views/dashboard/manage-organization/list/manage-org-list.component.ts
+++ src/app/views/dashboard/manage-organization/list/manage-org-list.component.ts
@@ -1,8 +1,15 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, OnChanges, Input} from '@angular/core';
 import { ManageOrganizationService } from 'app/services/dashboard/manage-organization.service';
 import { Observable } from 'rxjs/Observable';
 
 interface ManageHierarchy {
+    id:number;
+    name:string;
+    code:string;
+    hierarchy:number;
+};
+
+interface ManageOrganization {
     id:number;
     name:string;
     code:string;
@@ -16,26 +23,78 @@ interface ManageHierarchy {
 })
 export class ManageOrganizationListComponent implements OnInit {
     private isVisible:boolean;
-    private organizationListData:ManageHierarchy[];
-    
+    private organizationTypeListData :ManageHierarchy[];
+    private organizationListData :ManageOrganization[];
+    private selectOrg:any;
+    private settings:any;
+
     constructor(
         private manageOrgService:ManageOrganizationService
     ){        
-        
+    // ng-smart-table settings        
+    this.settings  =   {
+    delete:  {
+        confirmDelete:  true,
+    },
+    add:  {
+        confirmCreate:  true,
+    },
+    edit:  {
+        confirmSave:  true,
+    },
+    columns:  {
+        hierarchy:  {
+        title:   '#'
+        },
+        code:  {
+        title:   'Code'
+        },
+        name:  {
+        title:   'Name'
+        },
+        description:  {
+        title:   'Description'
+        },
+        active:  {
+        title:   'Status',
+        filter:  {
+            type:   'checkbox',
+            config:  {
+            true:   '1',
+            false:   '0',
+            resetText:   'clear',
+            },
+        },
+        }
+    }
+    };//end:settings
+    
     }//end:constructor
 
     selectOrganizationType(item:ManageHierarchy){
         console.log('Item selected is: ', item.name);
-    }//end:selectOrganizationType()
-
-    ngOnInit(){
-        this.manageOrgService.getAllOrganizationTypeConfig().subscribe((response) => {
+        this.manageOrgService.getAllOrganizationbyIdConfigById(item.id).subscribe((response) => {
             console.log('Response is: ', response);
             this.organizationListData = response;             
             },
             (error)=>{
-            this.isVisible = false;
+                this.isVisible = false;
             }); 
+            
+        }//end:selectOrganizationType()
+
+        
+        
+        ngOnInit(){
+            this.manageOrgService.getAllOrganizationTypeConfig().subscribe((response) => {
+                console.log('Response from GetAllOrganziation is: ', response);
+                this.organizationTypeListData = response;  
+                this.selectOrg = response[0];
+                this.selectOrganizationType(this.selectOrg);
+            },
+            (error)=>{
+            this.isVisible = false;
+            });         
     }//end:ngOnInit
 
 }//end:class-ManageOrganizationListComponent    
